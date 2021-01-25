8b0b4785321f990dfa232b228c134c3a9e10a9f5
amruta-kashikar <amrutakashikar2@gmail.com>
2021-01-25 22:43:51 +0530

    undo changes

--- app/components/dynamictable/dynamictable.js
+++ app/components/dynamictable/dynamictable.js
@@ -10,7 +10,6 @@ export default angular.module("rhGitHub.dynamictable", [])
     .component("dynamictable", {
         template: require("./dynamictable.html"),
         controller: "dynamictableCtrl"
-        regex : \b(?:(?!the)\w)+\b
     })
     .controller("dynamictableCtrl", ["$scope", function($scope) {
         // Initial
